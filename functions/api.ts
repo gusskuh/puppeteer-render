c2b873b5355b0a51bdab14b761564ff946e35b82
gusskuh <odedline@hotmail.com>
2024-11-05 13:23:51 +0200

    converting to ts

--- functions/api.ts
+++ functions/api.ts
@@ -11,7 +11,9 @@ console.log('Starting')
 
 
 router.get('/hello', (req, res) => res.json('Hello World!'));
-router.get('/', (req, res) => res.json('its working finally'));
+router.get('/', (req: any, res: any) => {
+    res.json('HELLO WORLD3');
+});
 
 app.use('/.netlify/functions/api', router);
 
